38fc5575f8ce6d9c45bf2f576b354580a3d9597e
gbibukitkalvariweb-arch <gbibukitkalvariweb@gmail.com>
2026-04-05 01:35:52 +0000

    Add: halaman detail renungan + fix navigasi

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar"; 
-import Footer from "./components/Footer"; 
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import TentangPage from "./pages/TentangPage";
 import IbadahPage from "./pages/IbadahPage";
-import LayananJemaatPage from "./pages/LayananJemaatPage"; 
+import LayananJemaatPage from "./pages/LayananJemaatPage";
 import RenunganPage from "./pages/RenunganPage";
 import CategoryPage from "./pages/CategoryPage";
-import RenunganDetailPage from "./pages/RenunganDetailPage";
 import EventPage from "./pages/EventPage";
 import PersembahanPage from "./pages/PersembahanPage";
+import RenunganDetailPage from "./pages/RenunganDetailPage";
 
 const App = () => {
   return (
@@ -26,9 +26,9 @@ const App = () => {
           <Route path="/layanan" element={<LayananJemaatPage />} />
           <Route path="/event" element={<EventPage />} />
           <Route path="/persembahan" element={<PersembahanPage />} />
-          <Route path="/kategori/:slug" element={<CategoryPage />} /> 
-          <Route path="/renungan" element={<RenunganPage />} /> 
+          <Route path="/renungan" element={<RenunganPage />} />
           <Route path="/renungan/:slug" element={<RenunganDetailPage />} />
+          <Route path="/kategori/:slug" element={<CategoryPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
