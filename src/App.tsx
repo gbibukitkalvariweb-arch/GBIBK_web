4344bd74bc869dd5c2548490f97f0735fea4e05e
gbibukitkalvariweb-arch <gbibukitkalvariweb@gmail.com>
2026-05-07 01:50:57 +0000

    fix cron schedule to hourly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -33,6 +34,7 @@ const App = () => {
         </Routes>
       </main>
       <Footer />
+      <Analytics />
     </BrowserRouter>
   );
 };
